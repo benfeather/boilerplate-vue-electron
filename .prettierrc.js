bf3cccc010849c84b5738bdd6b0cf6ffd9512231
Ben Feather <webmaster@benfeather.dev>
2021-06-06 19:30:06 +1200

    Fix html lint rules

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,10 @@
 module.exports = {
 	arrowParens: 'always',
 	bracketSpacing: true,
+	jsxBracketSameLine: false,
+	htmlWhitespaceSensitivity: 'ignore',
 	semi: false,
 	trailingComma: 'es5',
 	quoteProps: 'consistent',
-	vueIndentScriptAndStyle: true
+	vueIndentScriptAndStyle: true,
 }
