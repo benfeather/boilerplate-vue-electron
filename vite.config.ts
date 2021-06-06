985e9abbbec0a0f8ff4481b3fcc32ca34f75b809
Ben Feather <webmaster@benfeather.dev>
2021-06-06 17:24:55 +1200

    Initial

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { join } from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+const publicDir = join(__dirname, 'public')
+const appDir = join(__dirname, 'src/frontend')
+const outDir = join(__dirname, 'dist/frontend')
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  publicDir,
+  base: './',
+  plugins: [vue()],
+  build: {
+    outDir,
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      '@': appDir,
+    }
+  },
+})
