21669ecb57f6491f17c2a7df7848289594895eb0
Ben Feather <webmaster@benfeather.dev>
2021-06-07 19:26:40 +1200

    Update stderr, add comments

--- scripts/start.js
+++ scripts/start.js
@@ -6,14 +6,17 @@ const proc = require('process')
 	const electron = path.resolve('node_modules/.bin/electron')
 	const electronProc = cp.exec(`${electron} index.js`)
 
+	// Reroute stdout to the main process
 	electronProc.stdout.on('data', function (data) {
 		proc.stdout.write(data.toString())
 	})
 
+	// Reroute stderr to the main process
 	electronProc.stderr.on('data', function (data) {
-		proc.stdout.write(data.toString())
+		proc.stderr.write(data.toString())
 	})
 
+	// Restart the app if it exits with code 0
 	electronProc.on('exit', function (code) {
 		if (code !== 0) return
 
